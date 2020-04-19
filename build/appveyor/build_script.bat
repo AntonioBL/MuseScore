15a86f2eae041df1491e0ecedb6f7ecfa53b602c
AntonioBL <antonio.lotti@alice.it>
2020-04-19 11:19:52 +0200

    prova 2

--- build/appveyor/build_script.bat
+++ build/appveyor/build_script.bat
@@ -4,3 +4,8 @@ IF "%NIGHTLY_BUILD%" == "" (
 
 call C:\MuseScore\msvc_build.bat relwithdebinfo %TARGET_PROCESSOR_BITS% %APPVEYOR_BUILD_NUMBER%
 call C:\MuseScore\msvc_build.bat installrelwithdebinfo %TARGET_PROCESSOR_BITS% %APPVEYOR_BUILD_NUMBER%
+
+FOR %%I in (.) DO SET "BUILD_FOLDER=%%~nxI"
+
+ECHO ON
+ECHO Build folder = %BUILD_FOLDER%
